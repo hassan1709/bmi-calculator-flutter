1e5cb4a7422fa4587dfc29c4592c9d5903ede9c9
Hassan <hassan@appdone.net>
2020-05-20 20:34:12 +0800

    Screens and calculation of BMI

---
+++ lib/calculator_brain.dart
@@ -0,0 +1,39 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+
+class CalculatorBrain {
+  CalculatorBrain({@required this.weight, @required this.height}) {
+    calculateBMI();
+  }
+
+  final int height;
+  final int weight;
+  String _BMI;
+  String _result;
+  String _interpretation;
+
+  void calculateBMI() {
+    double bmi = weight / pow(height / 100, 2);
+    _BMI = bmi.toStringAsFixed(1);
+
+    if (bmi >= 25) {
+      _result = 'Overweight';
+      _interpretation =
+          'You have a higher than normal body weight. Try to exercise more.';
+    } else if (bmi >= 18.5) {
+      _result = 'Normal';
+      _interpretation = 'You have a normal body weight. Good job!';
+    } else {
+      _result = 'Underweight';
+      _interpretation =
+          'Your a lower than normal body weight. You can ear a bit more.';
+    }
+  }
+
+  String getBMI() => _BMI;
+
+  String getResult() => _result;
+
+  String getInterpretation() => _interpretation;
+}
---
+++ lib/constants.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+
+const kBottomContainerHeight = 80.0;
+const kActiveCardColour = Color(0xFF1D1E33);
+const kInActiveCardColour = Color(0xFF111328);
+const kBottomContainerColour = Color(0xFFEB1555);
+const kAlphaBottomContainerColour = Color(0x29EB1555);
+const kLabelStyleColour = Color(0xFF8D8E98);
+const kRoundedIconButtonColour = Color(0xFF4C4F5E);
+const kSliderMinValue = 100.0;
+const kSliderMaxValue = 250.0;
+
+const kLabelTextStyle = TextStyle(
+  fontSize: 18.0,
+  color: kLabelStyleColour,
+);
+
+const kNumberTextStyle = TextStyle(
+  fontSize: 50.0,
+  fontWeight: FontWeight.w900,
+);
+
+const kLargeButtonTextStyle = TextStyle(
+  fontSize: 25.0,
+  fontWeight: FontWeight.bold,
+);
+
+const kTitleTextStyle = TextStyle(
+  fontSize: 50.0,
+  fontWeight: FontWeight.bold,
+);
+
+const kResultTextStyle = TextStyle(
+  fontSize: 22.0,
+  fontWeight: FontWeight.bold,
+  color: Color(0xFF24D876),
+);
+
+const kBMITextStyle = TextStyle(
+  fontSize: 100.0,
+  fontWeight: FontWeight.bold,
+);
+
+const kBodyTextStyle = TextStyle(
+  fontSize: 22.0,
+  fontWeight: FontWeight.bold,
+);
